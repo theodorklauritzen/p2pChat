8898899d612a75c2a4282d173f62f091f169669a
Theodor Kvalsvik Lauritzen <theodor.k.lauritzen@gmail.com>
2019-04-28 17:42:58 +0200

    Added a Connect function

--- main.go
+++ main.go
@@ -2,9 +2,36 @@ package main
 
 import (
   "./p2pNetwork"
+  //"fmt"
+  "flag"
 )
 
+// main <PORT> <roomName> [password] [options]
+//
+// [-c <ip>]
+
 func main() {
-  p := p2pNetwork.InitPeer("room", "pass")
-  p.Listen(8080)
+
+  var port int
+  flag.IntVar(&port, "p", 8000, "listening port")
+
+  var roomName string
+  flag.StringVar(&roomName, "n", "", "The room name")
+
+  var roomPass string
+  flag.StringVar(&roomPass, "s", "", "The room name or room secret")
+
+  var connectIP string
+  flag.StringVar(&connectIP, "c", "", "Connect to another peer")
+
+  flag.Parse()
+
+  p := p2pNetwork.InitPeer(roomName, roomPass)
+  go p.Listen(port)
+
+  if connectIP != "" {
+    p.Connect(connectIP)
+  }
+
+  for {}
 }
--- p2pNetwork/p2pNetwork.go
+++ p2pNetwork/p2pNetwork.go
@@ -7,7 +7,7 @@ import (
   "encoding/binary"
 )
 
-const version uint = 1
+const version uint32 = 1
 
 type ThisPeer struct {
   roomName string
@@ -26,7 +26,7 @@ func (p ThisPeer) handleConnection(conn net.Conn) {
 
   // first package
   package1 := make([]byte, 4)
-  binary.LittleEndian.PutUint32(package1, uint32(version))
+  binary.LittleEndian.PutUint32(package1, version)
 
   roomNameBytes := []byte(p.roomName)
   package1 = append(package1, byte(len(roomNameBytes)))
@@ -34,15 +34,21 @@ func (p ThisPeer) handleConnection(conn net.Conn) {
 
   conn.Write(package1)
 
-  res1 := make([]byte, 256)
+  res1 := make([]byte, 1)
   conn.Read(res1)
-  password := string(res1[1:])
-  fmt.Println(password)
+
+  password := ""
+  if uint8(res1[0]) > 0 {
+    res1_2 := make([]byte, uint8(res1[0]))
+    conn.Read(res1_2)
+    password = string(res1_2)
+  }
 
   if (password == p.roomPassword) {
-    conn.Write([]byte{0x65, 0x65, 0x65})
+    conn.Write([]byte{0x80})
+    fmt.Println("A new peer successfully connected")
   } else {
-    conn.Write([]byte("Access Denied: wrong password"))
+    conn.Write([]byte{0x00})
   }
 }
 
@@ -58,5 +64,53 @@ func (p ThisPeer) Listen(port int) {
   		fmt.Println(err)
   	}
   	go p.handleConnection(conn)
+  }
+}
+
+func (p ThisPeer) Connect(peerIP string) {
+  conn, err := net.Dial("tcp", peerIP)
+  if err != nil {
+    fmt.Println(err)
+  }
+
+  rPackage1 := make([]byte, 5)
+  conn.Read(rPackage1)
+
+  rVersion := binary.LittleEndian.Uint32(rPackage1[:4])
+
+  if(rVersion != version) {
+    fmt.Println("failed to connect to: " + peerIP + ", due to wrong version")
+    conn.Close()
+  } else {
+    rRoomNameLength := int(rPackage1[4])
+    rPackage1_2 := make([]byte, rRoomNameLength)
+    conn.Read(rPackage1_2)
+
+    roomName := string(rPackage1_2)
+
+    if p.roomName == "" {
+      p.roomName = roomName
+    }
+    if p.roomName != roomName {
+      fmt.Println("The connected peer (" + peerIP + ") is member of the chat room: " + roomName + ", which does not correspond with the spesified name (" + p.roomName + ")")
+      fmt.Println("Stopping the connection")
+      conn.Close()
+    } else {
+      package2 := []byte{byte(len(p.roomPassword))}
+      package2 = append(package2, []byte(p.roomPassword)...)
+
+      conn.Write(package2)
+
+      rPackage3 := make([]byte, 1)
+      conn.Read(rPackage3)
+
+      if rPackage3[0] == 0x80 {
+        fmt.Println("successfully connected to " + p.roomName + " at " + peerIP)
+      } else if rPackage3[0] == 0x00 {
+        fmt.Println("Failed to connect to " + p.roomName + " at " + peerIP)
+        fmt.Println("This error can be caused by wrong password")
+        conn.Close()
+      }
+    }
   }
 }
